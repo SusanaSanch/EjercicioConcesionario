17906c91c26c99df525b14d56374adff9c4e8881
SusanaSanch <susanasanchez@auxiliamadrid.org>
2017-04-04 19:32:04 +0200

    concesionario Ampliado vehículos a camiones y añadido lectura y escritura de ficheros
    Empezado gui
    falla actualizar

--- src/concesionario/Menu.java
+++ src/concesionario/Menu.java
@@ -16,142 +16,79 @@ import javax.swing.JOptionPane;
  */
 public class Menu {
     
-    /**
-     * muestra el menú
-     */
-    public static void showMenu(){
-        
-        System.out.println("Seleccione una opción:\n");
-        System.out.println("1 - Crear nuevo coche");
-        System.out.println("2 - Listar todos los coches");
-        System.out.println("3 - Actualizar un coche");
-        System.out.println("4 - Borrar un coche");
-        System.out.println("0 - Salir");
+    static int opcion = 0;
+    static int id = 0;
+    static ArrayList<Vehiculo> listaVehiculos = new ArrayList<>();
+    
+     public static void escribirFichero()
+    {
+        System.out.println("Ha entrado en escribir fichero");
+        Codificador codificador = new Codificador();
+        GestorFicheros gestorFicheros = new GestorFicheros("objetos.txt");
+        String coded = codificador.codificar(listaVehiculos);
+        gestorFicheros.escribirFichero(coded);
     }
     
-    /**
-     * Recoge la opción elegida
-     * @return número de opción
-     */
-    public static int recogerRespuesta()
+    public static void leerFichero()
     {
-        int respuesta = 0;
+        Codificador codificador = new Codificador();
+        GestorFicheros gestorFicheros = new GestorFicheros("objetos.txt");
+        String objetosSerializados = gestorFicheros.leerFichero();
+        ArrayList<Vehiculo> objetos = codificador.descodificar(objetosSerializados);
+        /*
+        System.out.println("Marca\t\tModelo\t\tPrecio");
+        for (Vehiculo v : objetos) {
+            System.out.println(
+                v.getMarca()+"\t\t"+v.getModelo()+"\t\t"+v.getPrecio()
+            );
+        }*/
         
-        Scanner sc = new Scanner(System.in);
-        respuesta = Integer.valueOf(sc.nextLine());
-        
-        return respuesta;
     }
+    
+    public static void cargarEjemplos(){
+        //EJEMPLOS
+        int id = 4;
+        
+        Coche c1 = new Coche("Volskwagen", "T5", 12345, 1234, true);
+        Coche c2 = new Coche("Toyota", "Yaris", 6000, 25220, false);
+        Coche c3 = new Coche("Seat", "Alhambra", 10000, 100000, true);
+        
+        c1.setId(1);
+        c2.setId(2);
+        c3.setId(3);
+        
+        listaVehiculos.add(c1);
+        listaVehiculos.add(c2);
+        listaVehiculos.add(c3);
+        
+        Codificador codificador = new Codificador();
+        GestorFicheros gestorFicheros = new GestorFicheros("objetos.txt");
+        
+        //prueba escritura 
+        
+        String coded = codificador.codificar(listaVehiculos);
+        gestorFicheros.escribirFichero(coded);
+        
+        //prueba lectura
+        /*
+        String objetosSerializados = gestorFicheros.leerFichero();
+        ArrayList<Vehiculo> objetos = codificador.descodificar(objetosSerializados);
+        
+        System.out.println("Marca\t\tModelo\t\tPrecio");
+        for (Vehiculo v : objetos) {
+            System.out.println(
+                v.getMarca()+"\t\t"+v.getModelo()+"\t\t"+v.getPrecio()
+            );
+        }*/
+        
+        //FIN EJEMPLOS
+    }
+    
+   
     
     public static void salir()
     {
         System.out.println("Programa finalizado");
-    }
-    
-    public static void crearNuevoVehiculo(ArrayList<Vehiculo> listaVehiculos, int id)
-    {
-        int opcionVehiculo = Integer.valueOf(JOptionPane.showInputDialog("¿Qué vehículo quiere crear?\n"
-                + "1 - Coche\n"
-                + "2 - Camión"));
-        if(opcionVehiculo == 1)
-        {
-            Coche coche = new Coche();
-        
-            try {
-                coche.setId(id);
-                coche.setMarca(JOptionPane.showInputDialog("Introduzca marca del vehículo:"));
-                coche.setModelo(JOptionPane.showInputDialog("Introduzca modelo del vehículo:"));
-                coche.setPrecio(Float.valueOf(JOptionPane.showInputDialog("Introduzca precio del vehículo:")));
-                coche.setKilometraje(Integer.valueOf(JOptionPane.showInputDialog("Introduzca kilometraje:")));
-                coche.setSegundaMano(preguntaSegundaMano());
-
-            listaVehiculos.add(coche);
-            } catch (Exception e) {
-                System.out.println("Se ha producido un error creando el nuevo coche");
-            }
-        }else if(opcionVehiculo == 2)
-        {
-            Camion camion = new Camion();
-            try {
-                camion.setId(id);
-                camion.setMarca(JOptionPane.showInputDialog("Introduzca marca del vehículo:"));
-                camion.setModelo(JOptionPane.showInputDialog("Introduzca modelo del vehículo:"));
-                camion.setPrecio(Float.valueOf(JOptionPane.showInputDialog("Introduzca precio del vehículo:")));
-                camion.setCarga(Integer.valueOf(JOptionPane.showInputDialog("Introduzca la carga:")));
-                camion.setTipo(JOptionPane.showInputDialog("Introduzca tipo del vehículo:"));
-
-            listaVehiculos.add(camion);
-            } catch (Exception e) {
-                System.out.println("Se ha producido un error creando el nuevo coche");
-            }
-        }else
-        {
-            JOptionPane.showMessageDialog(null,"Valor introducido no válido");
-            crearNuevoVehiculo(listaVehiculos, id);
-        }
-    }
-    
-    public static boolean preguntaSegundaMano()
-    {
-        String booleano = "";
-        boolean segundaMano = false;
-        
-        booleano = JOptionPane.showInputDialog("¿El vehículo es de segunda mano? (SI/NO)");
-        
-        if(booleano.equalsIgnoreCase("SI"))
-        {
-            segundaMano = true;
-        }
-        else if(booleano.equalsIgnoreCase("NO"))
-        {
-            segundaMano = false;
-        }
-        else
-        {
-            System.out.println("El valor introducido no es válido");
-            preguntaSegundaMano();
-        }
-        
-        return segundaMano;
-    }
-    
-    public static void listarVehiculos(ArrayList<Vehiculo> listaVehiculos){
-        
-        for (Vehiculo vehiculo : listaVehiculos) {
-            if (vehiculo instanceof Coche)
-            {
-                System.out.println(((Coche)vehiculo).toString());
-            }
-            else
-            {
-                System.out.println(((Camion)vehiculo).toString());
-            }
-        }
-    }
-    
-    public static void actualizarVehiculo (ArrayList<Vehiculo> listaVehiculo)
-    {
-        
-        System.out.println("Elija un vehículo de la lista: ");
-        for(int i=1;i<listaVehiculo.size();i++){
-            System.out.println("opción"+i+" - "+listaVehiculo.get(i).toString());
-        }
-        int id = Integer.valueOf(JOptionPane.showInputDialog("Introduzca el vehículo a actualizar:"));
-        Vehiculo vehiculoDeseado = listaVehiculo.get(id);
-       
-        vehiculoDeseado.setMarca(JOptionPane.showInputDialog("Introduzca marca del vehículo:", vehiculoDeseado.getMarca()));
-        vehiculoDeseado.setModelo(JOptionPane.showInputDialog("Introduzca modelo del vehículo:", vehiculoDeseado.getModelo()));
-        vehiculoDeseado.setPrecio(Float.valueOf(JOptionPane.showInputDialog("Introduzca precio del vehículo:", vehiculoDeseado.getPrecio())));
-        
-        if(vehiculoDeseado instanceof Coche)
-        {
-            ((Coche) vehiculoDeseado).setKilometraje(Integer.valueOf(JOptionPane.showInputDialog("Introduzca kilometraje:", ((Coche) vehiculoDeseado).getKilometraje())));
-            ((Coche) vehiculoDeseado).setSegundaMano(preguntaSegundaMano());
-        }else
-        {
-            ((Camion)vehiculoDeseado).setCarga(Integer.valueOf(JOptionPane.showInputDialog("Introduzca carga:", ((Camion)vehiculoDeseado).getCarga())));
-            ((Camion)vehiculoDeseado).setTipo(JOptionPane.showInputDialog("Introduzca tipo:", ((Camion)vehiculoDeseado).getTipo()));
-        }
     }
     
     public static void borrarVehiculo(ArrayList<Vehiculo> listaVehiculo)
